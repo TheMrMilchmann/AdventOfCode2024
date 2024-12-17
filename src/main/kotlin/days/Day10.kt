5e22ec9ef7ba3687ea75b4fa4136147d37646da3
Leon Linhart <themrmilchmann@gmail.com>
2024-12-17 10:52:51 +0100

    feat(10): part 1

---
+++ src/main/kotlin/days/Day10.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Leon Linhart
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package days
+
+import utils.GridPos
+import utils.readInput
+import utils.toGrid
+
+fun main() {
+    val data = readInput().map { it.toCharArray().map(Char::digitToInt) }.toGrid()
+    val reachableEndsByPos = HashMap<GridPos, Set<GridPos>>(data.size)
+
+    val (ends, others) = data.positions.associateWith { data[it] }
+        .entries
+        .sortedByDescending { it.value }
+        .partition { it.value == 9 }
+
+    for (end in ends) {
+        reachableEndsByPos[end.key] = setOf(end.key)
+    }
+
+    var res = 0
+
+    for ((pos, v) in others) {
+        reachableEndsByPos[pos] =
+            (data.shiftLeft(pos)?.takeIf { data[it] == v + 1 }?.let(reachableEndsByPos::get) ?: emptySet()) +
+                    (data.shiftUp(pos)?.takeIf { data[it] == v + 1 }?.let(reachableEndsByPos::get) ?: emptySet()) +
+                    (data.shiftRight(pos)?.takeIf { data[it] == v + 1 }?.let(reachableEndsByPos::get) ?: emptySet()) +
+                    (data.shiftDown(pos)?.takeIf { data[it] == v + 1 }?.let(reachableEndsByPos::get) ?: emptySet())
+
+        if (v == 0) res += reachableEndsByPos[pos]!!.count()
+    }
+
+    println("Part 1: $res")
+}
